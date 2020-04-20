deefc8c453d93685fd7d28b6815c7864c2b1cc19
Ignacio <belmonteperona@gmail.com>
2020-04-20 21:12:19 +0200

    implemented new method from bakend, no tested

--- src/app/services/posts.service.spec.ts
+++ src/app/services/posts.service.spec.ts
@@ -1,12 +1,90 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, inject } from '@angular/core/testing';
 
 import { PostsService } from './posts.service';
+import { HttpClientModule } from '@angular/common/http';
+import { IonicStorageModule } from '@ionic/storage';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { RouterModule } from '@angular/router';
+import { FileTransfer } from '@ionic-native/file-transfer/ngx';
+import { Post } from '../interfaces/interfaces';
+import { CompileMetadataResolver } from '@angular/compiler';
+import { HttpTestingController, HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('PostsService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+
+  let service: PostsService;
+  let httpMock: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule, IonicStorageModule.forRoot(), RouterModule.forRoot([])],
+      providers: [PostsService, Geolocation, FileTransfer]
+    })
+    service = TestBed.get(PostsService);
+    httpMock = TestBed.get(HttpTestingController);
+
+  });
 
   it('should be created', () => {
-    const service: PostsService = TestBed.get(PostsService);
     expect(service).toBeTruthy();
   });
-});
+
+  describe('#getPosts()', () => {
+    it('Should get the last 10 post', () => {
+      service.getPosts().subscribe((data) => {
+        expect(data).toBeDefined();
+        expect(data.posts.length).toEqual(10);
+      })
+    });
+
+    it('Should return the 10 next post', () => {
+      service.getPosts(true).subscribe((data) => {
+        service.getPosts().subscribe((data2) => {
+          expect(data).toBeDefined();
+          expect(data2).toBeDefined();
+          // expect(data.posts.length).toEqual(10);
+          // expect(data2.posts.length).toEqual(10);
+          for (let i = 0; i < data.posts.length; i++) {
+            expect(data.posts[i]._id).not.toEqual(data2.posts[i]._id);
+          }
+        })
+      })
+    });
+  });
+
+  describe('#getPost()', () => {
+    it('Should get post', () => {
+      service.getPosts().subscribe((data) => {
+        service.getPost(data.posts[0]._id).subscribe((datapost: any) => {
+          expect(datapost).toBeDefined();
+          expect(data).toBeDefined();
+          expect(datapost.post).toEqual(data.posts[0]);
+        })
+      })
+    });
+
+    it('Should return ok false, post id not exist', () => {
+      service.getPost('123').subscribe((datapost: any) => {
+        expect(datapost).toBeDefined();
+        expect(datapost.ok).toEqual(false);
+      })
+    });
+  });
+
+  describe('delete Post', () => {
+    it('Shold delete the last post posted', () => {
+      service.getPosts().subscribe(async (data) => {
+        const resp = await service.deletePost(data.posts[0]._id)
+        expect(resp).toEqual(true);
+        service.getPosts(true).subscribe(async (dataUpdated) => {
+          expect(data.posts[0]).not.toEqual(dataUpdated.posts[0])
+        })
+      })
+    });
+
+    it('Shold return false, id post incorrect', async () => {
+      const resp = await service.deletePost('1234');
+      expect(resp).toEqual(false);
+    });
+  });
+})
--- src/app/services/posts.service.ts
+++ src/app/services/posts.service.ts
@@ -1,12 +1,12 @@
 import { Injectable, EventEmitter } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from '../../environments/environment.prod';
-import { RespuestaPosts, Post } from '../interfaces/interfaces';
+import { RespuestaPosts, Post, RespuestaSubidaImagen } from '../interfaces/interfaces';
 import { UsuarioService } from './usuario.service';
 import { FileTransfer, FileUploadOptions, FileTransferObject } from '@ionic-native/file-transfer/ngx';
 import { resolve } from 'url';
 
-const url = environment.url;
+const url = `${environment.url}/post`;
 
 @Injectable({
   providedIn: 'root'
@@ -22,16 +22,16 @@ export class PostsService {
 
   nuevoPost = new EventEmitter<Post>()
 
-  crearPost(post) {
+  //new post
+  newPost(post: Post) {
     return new Promise(resolve => {
       const headers = new HttpHeaders({
         'x-token': this.usuarioService.token
       })
-      this.http.post(`${url}/post`, post, { headers }).subscribe(data => {
-        console.log(data);
+      this.http.post(`${url}`, post, { headers }).subscribe(data => {
         if (data['ok']) {
-          this.nuevoPost.emit(data['post']);
-          return resolve(true);
+          this.nuevoPost.emit(data['post'])
+          return resolve(data);
         } else {
           return resolve(false);
         }
@@ -39,41 +39,137 @@ export class PostsService {
     });
   }
 
-  getPosts(pull: boolean = false) {
-    if (pull) {
+  // Get posts on pages from 10 post
+  getPosts(reset: boolean = false) {
+    if (reset) {
       this.paginaPosts = 0;
     }
     this.paginaPosts++;
-    return this.http.get<RespuestaPosts>(`${url}/post/?pagina=${this.paginaPosts}`)
+    return this.http.get<RespuestaPosts>(`${url}/?pagina=${this.paginaPosts}`);
   }
 
-  likePost(idPost:string){
-    return new Promise(resolve=>{
+  //Get post by id
+  getPostByid(idPost: string) {
+    return this.http.get<RespuestaPosts>(`${url}/get/${idPost}`);
+  }
+
+  //Delete post
+  deletePost(idPost: string) {
+    return new Promise((resolve) => {
       const headers = new HttpHeaders({
         'x-token': this.usuarioService.token
       })
-      this.http.post(`${url}/post/like/${idPost}`,null,{headers}).subscribe((data:any)=>{
-        if(data['ok']){
+      this.http.delete<RespuestaPosts>(`${url}/remove/${idPost}`, { headers }).subscribe(data => {
+        if (data['ok']{
+          console.log(data)
+          return resolve(true)
+        } else {
+          return resolve(false)
+        }
+      });
+    })
+  }
+
+  //Delete temp file
+  deleteTempFile(imageName: string) {
+    const headers = new HttpHeaders({
+      'x-token': this.usuarioService.token
+    })
+    return new Promise((resolve) => {
+      this.http.delete(`${url}/image/temp/${imageName}`, { headers }).subscribe((data) => {
+        if (data[`ok`]) {
+          resolve(data);
+        } else {
+          resolve(false);
+        }
+      })
+    })
+  }
+
+  //Delete temp folder
+  deleteTempFolder() {
+    const headers = new HttpHeaders({
+      'x-token': this.usuarioService.token
+    })
+    return new Promise(resolve => {
+      this.http.delete(`${url}/image/temp`, { headers }).subscribe(data => {
+        if (data['ok']) {
+          return resolve(data);
+        } else {
+          return resolve(false);
+        }
+      })
+    })
+  }
+
+  // like/dislike post
+  likePost(idPost: string) {
+    return new Promise(resolve => {
+      const headers = new HttpHeaders({
+        'x-token': this.usuarioService.token
+      })
+      this.http.post(`${url}/post/like/${idPost}`, null, { headers }).subscribe((data: any) => {
+        if (data['ok']) {
           return resolve(data.postDB);
-        }else{
+        } else {
           return resolve(false)
         }
       })
     })
   }
 
-  subirImagen(pathImg: string) {
-    const options: FileUploadOptions = {
-      fileKey: 'imagen',
-      headers: {
-        'x-token': this.usuarioService.token
-      }
-    };
+  //Get Image post
+  uploadImage(pathImg: string) {
+    return new Promise(resolve => {
+      const options: FileUploadOptions = {
+        fileKey: 'imagen',
+        headers: {
+          'x-token': this.usuarioService.token
+        }
+      };
+      const fileTransfer: FileTransferObject = this.fileTransefer.create();
+      fileTransfer.upload(pathImg, `${url}/post/upload`, options).then((data: any) => {
+        resolve(JSON.parse(data.response))
+      }).catch(console.log)
 
-    const fileTransfer: FileTransferObject = this.fileTransefer.create();
-    fileTransfer.upload(pathImg, `${url}/post/upload`, options).then(data => {
-      console.log(data)
-    }).catch(console.log)
+    })
+  }
+
+  //Add comment to post
+  addCommentToPost(commentText: string, idPost: string) {
+    const headers = new HttpHeaders({
+      'x-token': this.usuarioService.token
+    })
+    return new Promise(resolve => {
+      this.http.post(`${url}/comment/${idPost}`, { text: commentText }, { headers }).subscribe(data => {
+        if (data['ok']) {
+          return resolve(data['post'])
+        } else {
+          return resolve(false);
+        }
+      })
+    })
+  }
+
+  //delete comment
+  deleteComment(idPost: string, idComment: string) {
+    const headers = new HttpHeaders({
+      'x-token': this.usuarioService.token
+    })
+    return new Promise(resolve => {
+      this.http.delete(`${url}/comment/${idPost}/${idComment}`, { headers }).subscribe(data => {
+        if (data['ok']) {
+          resolve(data['post']);
+        } else {
+          resolve(false);
+        }
+      })
+    });
   }
 
 }
+
+
+
+
+
